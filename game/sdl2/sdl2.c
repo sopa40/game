8415db8a086c1d6b210bc18ddf1bc8087d4cd85a
Nazar Sopiha <sopin99@llangollen.imp.fu-berlin.de>
2019-04-02 16:34:54 +0200

    randomly positioned stars added. image lib included

--- game/sdl2/sdl2.c
+++ game/sdl2/sdl2.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 2
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_log.h>
+#include "SDL2/SDL_image.h"
 #include <sys/types.h>
 #include <unistd.h>
 #include <assert.h>
@@ -11,6 +12,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 /* Configurable items */
 #define CONFIG_PROGNAME sdl2_test
@@ -25,18 +28,17 @@
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define min(a, b) ((a) <= (b) ? (a) : (b))
 
-/* Global constants, typed for non-strings, macros for some strings to use compile-time concat */
+/* Global constants, typed for non-strings, macros for some strings to use compile-time concat
 #define PROGNAME xstr(CONFIG_PROGNAME)
 static const int POLL_RATE_MS = CONFIG_POLL_RATE_MS;
 static const int WINDOW_WIDTH = CONFIG_WINDOW_WIDTH;
-static const int WINDOW_HEIGHT = CONFIG_WINDOW_HEIGHT;
+static const int WINDOW_HEIGHT = CONFIG_WINDOW_HEIGHT; */
 
 /* Global variables, shouldn't be many */
 
 /* Handles events
  * returns -1 for quit event
  */
-
 
 
 
@@ -47,7 +49,41 @@ typedef struct
     char *name;
 } Man;
 
-void render(SDL_Renderer *renderer, Man *man)
+typedef struct
+{
+    int x, y;
+} Star;
+
+typedef struct
+{
+
+    Man man;
+    Star stars[10];
+    SDL_Texture *star;
+    SDL_Renderer *renderer;
+
+} Gamestate;
+
+void loadGame(Gamestate *game){
+    SDL_Surface *starSurface = NULL;
+    starSurface = IMG_Load("star.png");
+    if(starSurface == NULL){
+        printf("Can not find star.png!\n");
+        SDL_Quit();
+        exit(1);
+    }
+    game->star = SDL_CreateTextureFromSurface(game->renderer, starSurface);
+    SDL_FreeSurface(starSurface);
+    game->man.x = 220;
+    game->man.y = 140;
+
+    for(int i = 0; i < 10; i++){
+        game->stars[i].x = random()%640;
+        game->stars[i].y = random()%480;
+    }
+}
+
+void render(SDL_Renderer *renderer, Gamestate *game)
 {
     SDL_SetRenderDrawColor(renderer, 0, 0 ,255, 255);
 
@@ -55,13 +91,20 @@ void render(SDL_Renderer *renderer, Man *man)
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    SDL_Rect rect = { man->x, man->y, 200, 200 };
+    SDL_Rect rect = { game->man.x, game->man.y, 100, 100 };
     SDL_RenderFillRect(renderer, &rect);
 
+
+    for(int i = 0; i < 10; i++){
+        SDL_Rect starRect = { game->stars[i].x, game->stars[i].y, 64, 64};
+        SDL_RenderCopy(renderer, game->star, NULL, &starRect);
+
+    }
     SDL_RenderPresent(renderer);
+
 }
 
-int processEvents(SDL_Window *window, Man *man)
+int processEvents(SDL_Window *window, Gamestate *game)
 {
     SDL_Event event;
     int done = 0;
@@ -85,18 +128,6 @@ int processEvents(SDL_Window *window, Man *man)
                     case SDLK_ESCAPE:
                         done = 1;
                     break;
-                    case SDLK_RIGHT:
-                        man->x += 10;
-                    break;
-                    case SDLK_LEFT:
-                        man->x += -10;
-                    break;
-                    case SDLK_DOWN:
-                        man->y += +10;
-                    break;
-                    case SDLK_UP:
-                        man->y += -10;
-                    break;
                 }
                 break;
                 case SDL_QUIT:
@@ -105,20 +136,26 @@ int processEvents(SDL_Window *window, Man *man)
             }
         }
     }
-
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+    if(state[SDL_SCANCODE_LEFT])
+        game->man.x -= 10;
+    if(state[SDL_SCANCODE_RIGHT])
+        game->man.x += 10;
+    if(state[SDL_SCANCODE_DOWN])
+        game->man.y += 10;
+    if(state[SDL_SCANCODE_UP])
+        game->man.y -= 10;
     return done;
 }
 
-int main(int argc, char *argv[])
+int main(void)
 {
+    Gamestate game;
     SDL_Window *window;
     SDL_Renderer *renderer;
 
     SDL_Init(SDL_INIT_VIDEO);
-
-    Man man;
-    man.x = 220;
-    man.y = 140;
+    srandom((int)time(NULL));
 
     window = SDL_CreateWindow("Game Window",
                               SDL_WINDOWPOS_UNDEFINED,
@@ -128,20 +165,21 @@ int main(int argc, char *argv[])
                               0
                               );
 
+
+
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    game.renderer = renderer;
+    loadGame(&game);
 
     int done = 0;
 
     while(!done){
-        done = processEvents(window, &man);
-
-        render(renderer,&man);
-
+        done = processEvents(window, &game);
+        render(renderer,&game);
         SDL_Delay(100);
-
     }
 
-
+    SDL_DestroyTexture(game.star);
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
 
